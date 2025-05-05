d689c2ed10151814c67c8f3bf2048406fa24f8fd
unknown <alejandromerlina324@gmail.com>
2025-05-05 08:23:22 -0400

    follow

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=80)
+    description=models.TextField()
+    price=models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.1)]
+    )
+    stock=models.PositiveIntegerField()
+    image=models.ImageField(upload_to='static/products')
