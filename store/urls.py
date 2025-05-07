9e065de40d36d0c73575a100eaa5ba945ed7c15c
unknown <josecastillo@gmail.com>
2025-05-07 17:31:22 -0400

    finish detail

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from store.views import ProductListView
+from store.views import *
 
 urlpatterns=[
 
-path("",ProductListView.as_view(),name="product_list")
-
+path("",ProductListView.as_view(),name="product_list"),
+path("<int:pk>",ProductDetailView.as_view(),name="detail")
 ]
+
--- store/views.py
+++ store/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
 class ProductListView(ListView):
     model=Product
     template_name="list_products.html"
     context_object_name="products"
+
+class ProductDetailView(DetailView):
+    model=Product
+    template_name="product_detail.html"
+    context_object_name="product"
