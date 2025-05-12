34a4150a85d1732e708f298e855bb5d7b3e7ef0c
unknown <josecastillo@gmail.com>
2025-05-12 17:31:50 -0400

    finish create view

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from store.views import *
 urlpatterns=[
 
 path("",ProductListView.as_view(),name="product_list"),
-path("<int:pk>",ProductDetailView.as_view(),name="detail")
+path("<int:pk>",ProductDetailView.as_view(),name="detail"),
+path("create",ProductCreateView.as_view(),name="create"),
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from store.models import Product
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class ProductListView(ListView):
@@ -12,3 +13,9 @@ class ProductDetailView(DetailView):
     model=Product
     template_name="product_detail.html"
     context_object_name="product"
+
+class ProductCreateView(CreateView):
+    model=Product
+    template_name="product_create.html"
+    fields="__all__"
+    success_url=reverse_lazy("product_list")
