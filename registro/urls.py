7c59a461983df5eb57246670c310ef216f6564b4
unknown <josecastillo@gmail.com>
2025-05-19 17:51:40 -0400

    finish Signup

---
+++ registro/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path,include 
+from .views import *
+urlpatterns=[
+    path("accounts",include("django.contrib.auth.urls")),
+    path("",SignUpView.as_view(),name="signup")
+]
